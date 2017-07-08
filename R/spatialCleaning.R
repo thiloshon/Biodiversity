8622c7fa668b3bf1d593b425bba64641943745c7
Thiloshon Nagarajah <thiloshon@gmail.com>
2017-07-08 21:22:18 +0530

    Spatial First 10 Checks

--- R/spatialCleaning.R
+++ R/spatialCleaning.R
@@ -2,7 +2,8 @@ coordinatesDecimalMismatch <- function(GBIF_DataFrame) {
     t <- Sys.time()
     lat <- sapply(GBIF_DataFrame$decimalLatitude, function(lat) {
         list <- strsplit(sub('0+$', '', as.character.numeric_version(lat)),
-                         ".", fixed = TRUE)
+                         ".",
+                         fixed = TRUE)
 
         if (length(list[[1]]) < 2) {
             return (0)
@@ -13,7 +14,8 @@ coordinatesDecimalMismatch <- function(GBIF_DataFrame) {
 
     long <- sapply(GBIF_DataFrame$decimalLongitude, function(long) {
         list <- strsplit(sub('0+$', '', as.character.numeric_version(long)),
-                         ".", fixed = TRUE)
+                         ".",
+                         fixed = TRUE)
         if (length(list[[1]]) < 2) {
             return (0)
         } else {
@@ -28,58 +30,198 @@ coordinatesDecimalMismatch <- function(GBIF_DataFrame) {
 }
 
 
-repeatingDigits <- function(GBIF_Data){
+repeatingDigits <- function(GBIF_Data) {
     t <- Sys.time()
 
 
-    latRepeat <- sapply(GBIF_Data$decimalLatitude, function(lat){
+    latRepeat <- sapply(GBIF_Data$decimalLatitude, function(lat) {
         x <- as.character.numeric_version(lat)
         rmword <- grepl("(\\w)\\1{2, }", x)
         return(rmword)
     })
 
-    longRepeat <- sapply(GBIF_Data$decimalLongitude, function(long){
-        x <- as.character.numeric_version(long)
-        rmword <- grepl("(\\w)\\1{2, }", x)
-        return(rmword)
-    })
+    longRepeat <-
+        sapply(GBIF_Data$decimalLongitude, function(long) {
+            x <- as.character.numeric_version(long)
+            rmword <- grepl("(\\w)\\1{2, }", x)
+            return(rmword)
+        })
 
     require(data.table)
 
-    t1 <- sapply(1:dim(GBIF_Data)[1], function(counter){
-        if(latRepeat[counter]){
-            lat <- as.character.numeric_version(GBIF_Data[counter,c("decimalLatitude")])
+    GBIF_Data$latRepeatCount  <-
+        sapply(1:dim(GBIF_Data)[1], function(counter) {
+            if (latRepeat[counter]) {
+                lat <-
+                    as.character.numeric_version(GBIF_Data[counter, c("decimalLatitude")])
 
-            list = as.vector(strsplit(lat, ""))
-            table <- as.data.table(list)
-            frameCount <- table[, count := sequence(.N), by = rleid(V1)][V1 == "No", count := 0][]
+                list = as.vector(strsplit(lat, ""))
+                table <- as.data.table(list)
+                frameCount <-
+                    table[, count := sequence(.N), by = rleid(V1)][V1 == "No", count := 0][]
 
-            max(frameCount$count)
-        }else{
-            0
-        }
-    })
+                max(frameCount$count)
+            } else{
+                0
+            }
+        })
 
-    GBIF_Data$latRepeatCount <- t1
 
-    t2 <- sapply(1:dim(GBIF_Data)[1], function(counter){
-        if(longRepeat[counter]){
-            long <- as.character.numeric_version(GBIF_Data[counter,c("decimalLongitude")])
+    GBIF_Data$longRepeatCount <-
+        sapply(1:dim(GBIF_Data)[1], function(counter) {
+            if (longRepeat[counter]) {
+                long <-
+                    as.character.numeric_version(GBIF_Data[counter, c("decimalLongitude")])
 
-            list = as.vector(strsplit(long, ""))
-            table <- as.data.table(list)
-            frameCount <- table[, count := sequence(.N), by = rleid(V1)][V1 == "No", count := 0][]
+                list = as.vector(strsplit(long, ""))
+                table <- as.data.table(list)
+                frameCount <-
+                    table[, count := sequence(.N), by = rleid(V1)][V1 == "No", count := 0][]
 
-            max(frameCount$count)
-        }else{
-            0
-        }
-    })
-
-    GBIF_Data$longRepeatCount <- t2
+                max(frameCount$count)
+            } else{
+                0
+            }
+        })
 
     print(Sys.time() - t)
     return(GBIF_Data)
 
 
+}
+
+georeferenceProtocolFlag <- function(GBIF_Data) {
+    # > sumFac(y$georeferenceProtocolFlag)
+    # High     Low  Medium    NA's
+    #  24     409    3961 1753799
+
+    t <- Sys.time()
+    values <-
+        c(
+            "",
+            "MaNIS/HerpNet/ORNIS Georeferencing Guidelines, GBIF Best Practices",
+            "GEO MACN v. 0.1",
+            "not recorded",
+            "VertNet Georeferencing Guidelines",
+            "MaNIS georeferencing guidelines" ,
+            "Collector assigned",
+            "GEOLocate",
+            "unknown-migration",
+            "collector",
+            "MaNIS",
+            "unknown",
+            "digital resource",
+            "unspecified",
+            "GeoLocate",
+            "BioGeoMancer",
+            "GPS",
+            "estimated from other samples"
+        )
+
+    ## TODO: refine this list after input from experts
+    flags <- c(
+        NA,
+        "Medium",
+        "Medium",
+        "Low",
+        "Medium",
+        "Medium",
+        "Low",
+        "Medium",
+        "Low",
+        "Low",
+        "Medium",
+        NA,
+        "Medium",
+        NA,
+        "Medium",
+        "Medium",
+        "High",
+        "Low"
+    )
+
+    GBIF_Data$georeferenceProtocolFlag <-
+        flags[match(GBIF_Data$georeferenceProtocol, values)]
+
+    print(Sys.time() - t)
+
+    return(GBIF_Data)
+
+}
+
+georeferenceVerificationStatusFlag <- function(GBIF_Data) {
+    # > sumFac(y$georeferenceVerificationStatusFlag)
+    # High     Low    NA's
+    #  22    3949 1754222
+
+    t <- Sys.time()
+    values <-
+        c(
+            "",
+            "not georeferenced",
+            "Not verified",
+            "requires verification",
+            "unknown",
+            "unverified" ,
+            "verified by curator"
+        )
+
+    ## TODO: refine this list after input from experts
+    flags <- c(NA,
+               "Low",
+               "Low",
+               "Low",
+               NA,
+               "Low",
+               "High")
+
+    GBIF_Data$georeferenceVerificationStatusFlag <-
+        flags[match(GBIF_Data$georeferenceVerificationStatus, values)]
+
+    print(Sys.time() - t)
+
+    return(GBIF_Data)
+
+}
+
+georeferencePostOccurrenceFlag <- function(GBIF_Data) {
+    t <- Sys.time()
+
+    require(parsedate)
+
+    logical <- GBIF_Data$georeferencedDate != ""
+    subset <- GBIF_Data[logical, ]
+
+    eventDate <- parse_date(subset$eventDate)
+    referencedDate <- parse_date(subset$georeferencedDate)
+
+    diffInYears <- as.numeric(referencedDate - eventDate) / 365.242
+    GBIF_Data$georeferencePostOccurrenceFlag <- NA
+    GBIF_Data$georeferencePostOccurrenceFlag[logical] <- diffInYears
+
+    print(Sys.time() - t)
+
+    return(GBIF_Data)
+}
+
+coordinatePrecisionOutofRangeFlag <- function(GBIF_Data) {
+    t <- Sys.time()
+
+    GBIF_Data$coordinatePrecisionOutofRangeFlag <-
+        GBIF_Data$coordinatePrecision < 0 | GBIF_Data$coordinatePrecision > 1
+
+    print(Sys.time() - t)
+
+    return(GBIF_Data)
+}
+
+uncertaintyOutofRangeFlag <- function(GBIF_Data) {
+    t <- Sys.time()
+
+    GBIF_Data$uncertaintyOutofRangeFlag <-
+        GBIF_Data$coordinateUncertaintyInMeters%%1!=0 | GBIF_Data$coordinateUncertaintyInMeters < 0
+
+    print(Sys.time() - t)
+
+    return(GBIF_Data)
 }
