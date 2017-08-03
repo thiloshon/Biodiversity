0546ad19b5d60f9f255e4dfba7c8d1506b161bd3
Thiloshon Nagarajah <thiloshon@gmail.com>
2017-08-03 19:19:03 +0530

    Add files via upload

--- R/Grouping.R
+++ R/Grouping.R
@@ -3,12 +3,12 @@ addPartition <- function(tree = NULL, grouping, applyTo = "leaf", column = NULL,
     if (is.null(tree)) {
         tree <- Node$new("Partition Tree")
     }
-    
+
     if (grouping == "all") {
         if (applyTo == "leaf") {
             for (x in 1:length(classes)) {
                 if (x == 1) {
-                  treeNew <- Node$new("Partition Tree")
+                    treeNew <- Node$new("Partition Tree")
                 }
                 treeNew <- addPartition(treeNew, grouping = classes[x], applyTo = "root")
             }
@@ -18,15 +18,15 @@ addPartition <- function(tree = NULL, grouping, applyTo = "leaf", column = NULL,
                 addPartition(tree, grouping = classes[x], applyTo = "root")
             }
         }
-        
+
         return(tree)
     }
-    
+
     if (grouping == "primary3") {
         if (applyTo == "leaf") {
             for (x in 1:3) {
                 if (x == 1) {
-                  treeNew <- Node$new("Partition Tree")
+                    treeNew <- Node$new("Partition Tree")
                 }
                 treeNew <- addPartition(treeNew, grouping = classes[x], applyTo = "root")
             }
@@ -36,10 +36,10 @@ addPartition <- function(tree = NULL, grouping, applyTo = "leaf", column = NULL,
                 addPartition(tree, grouping = classes[x], applyTo = "root")
             }
         }
-        
+
         return(tree)
     }
-    
+
     if (grepl("greaterThan", grouping)) {
         # print('hi')
         value <- as.integer(gsub("greaterThan", "", grouping))
@@ -56,13 +56,13 @@ addPartition <- function(tree = NULL, grouping, applyTo = "leaf", column = NULL,
         }
         return(tree)
     }
-    
+
     if (grouping == "column") {
         if (length(strsplit(column, "~")[[1]]) == 2) {
             field <- strsplit(column, "~")[[1]][1]
             value <- strsplit(column, "~")[[1]][2]
-            
-            
+
+
             if (applyTo == "leaf") {
                 treeNew <- Node$new("Partition Tree")
                 treeNew <- addPartition(treeNew, grouping = paste(field, "=", value, sep = ""), applyTo = "root")
@@ -74,31 +74,31 @@ addPartition <- function(tree = NULL, grouping, applyTo = "leaf", column = NULL,
                 addPartition(tree, grouping = paste(field, "=", value, sep = ""), applyTo = "root")
                 addPartition(tree, grouping = paste(field, "!=", value, sep = ""), applyTo = "root")
             }
-            
+
         } else {
             column <- strsplit(column, "~")[[1]]
             # print(column)
             values <- unique(data[, column])
             if (applyTo == "leaf") {
                 for (x in 1:length(values)) {
-                  # print(x)
-                  if (x == 1) {
-                    treeNew <- Node$new("Partition Tree")
-                  }
-                  treeNew <- addPartition(treeNew, grouping = values[x], applyTo = "root")
+                    # print(x)
+                    if (x == 1) {
+                        treeNew <- Node$new("Partition Tree")
+                    }
+                    treeNew <- addPartition(treeNew, grouping = values[x], applyTo = "root")
                 }
                 tree$Do(addPartitionToLeaf, value = treeNew, filterFun = isLeaf)
             } else if (applyTo == "root") {
                 for (x in 1:length(values)) {
-                  addPartition(tree, grouping = values[x], applyTo = "root")
+                    addPartition(tree, grouping = values[x], applyTo = "root")
                 }
             }
-            
+
         }
-        
+
         return(tree)
     }
-    
+
     if (applyTo == "leaf") {
         tree$Get(addPartitionToLeaf, value = grouping, filterFun = isLeaf)
     } else if (applyTo == "root") {
@@ -113,7 +113,7 @@ listPartitions <- function(tree, onlyLeaf = FALSE) {
     } else {
         operations <- as.vector(tree$Get("pathString", traversal = "level"))
     }
-    
+
     fix <- sapply(operations, function(string) {
         gsub("/", " -> ", string)
     })
@@ -132,51 +132,51 @@ applyPartitions <- function(tree, data, return = "list") {
                 data <- data[, c(get(groups[counter])[logic])]
             }
             # ---------- End of class groupings ------------ #
-            
+
             # ---------- all grouping | Column ------------ #
             if (groups[counter] == "all") {
                 logic <- get(groups[counter]) %in% names(data)
                 data <- data[, c(get(groups[counter])[logic])]
             }
-            
-            
-            
+
+
+
             if (grepl("GreaterThan", groups[counter])) {
                 value <- as.integer(gsub("GreaterThan", "", groups[counter]))
                 log1 <- which(colMeans(is.na(data)) > value/10)
                 log2 <- which(colMeans(data != "") > value/10)
-                
+
                 log <- any(log1, log2)
-                
+
                 return(data[-log, ])
             }
-            
+
             if (grepl("LessThan", groups[counter])) {
                 value <- as.integer(gsub("LessThan", "", groups[counter]))
                 log1 <- which(colMeans(is.na(data)) > value/10)
                 log2 <- which(colMeans(data != "") > value/10)
-                
+
                 log <- any(log1, log2)
-                
+
                 return(data[log, ])
             }
-            
+
         }
-        
+
         return(data)
-        
+
     })
-    
+
     names(results) <- operations
-    
+
     if (return == "dataframe") {
         g <- bind_cols(results[2:length(results)])
         temp <- g[, !duplicated(colnames(g))]
         return(temp)
     }
-    
+
     return(results)
-    
+
 }
 
 
@@ -190,9 +190,9 @@ addPartitionToLeaf <- function(node, value = NULL) {
         for (x in 1:length(value$children)) {
             node$AddChild(value$children[[x]]$name)
         }
-        
+
     }
-    
+
 }
 
 getPartitions <- function(tree) {
@@ -201,47 +201,45 @@ getPartitions <- function(tree) {
 
 # ------------------------ Grouping Classes ------------------------------#
 
-classes <- c("taxonClass", "locationClass", "eventClass", "occurenceClass", "recordlevelTermsClass", "geologicalContextClass", 
-    "identificationClass", "resourceRelationshipClass", "measurementOrFactClass")
+classes <- c("taxonClass", "locationClass", "eventClass", "occurenceClass", "recordlevelTermsClass", "geologicalContextClass",
+"identificationClass", "resourceRelationshipClass", "measurementOrFactClass")
 
+taxonClass <- c("taxonID", "scientificNameID", "acceptedNameUsageID", "parentNameUsageID", "originalNameUsageID", "nameAccordingToID",
+"namePublishedInID", "taxonConceptID", "scientificName", "acceptedNameUsage", "parentNameUsage", "originalNameUsage", "nameAccordingTo",
+"namePublishedIn", "namePublishedInYear", "higherClassification", "kingdom", "phylum", "class", "order", "family", "genus",
+"subgenus", "specificEpithet", "infraspecificEpithet", "taxonRank", "verbatimTaxonRank", "scientificNameAuthorship", "vernacularName",
+"nomenclaturalCode")
 
-taxonClass <- c("taxonID", "scientificNameID", "acceptedNameUsageID", "parentNameUsageID", "originalNameUsageID", "nameAccordingToID", 
-    "namePublishedInID", "taxonConceptID", "scientificName", "acceptedNameUsage", "parentNameUsage", "originalNameUsage", "nameAccordingTo", 
-    "namePublishedIn", "namePublishedInYear", "higherClassification", "kingdom", "phylum", "class", "order", "family", "genus", 
-    "subgenus", "specificEpithet", "infraspecificEpithet", "taxonRank", "verbatimTaxonRank", "scientificNameAuthorship", "vernacularName", 
-    "nomenclaturalCode")
+recordlevelTermsClass <- c("institutionID", "collectionID", "datasetID", "institutionCode", "collectionCode", "datasetName", "ownerInstitutionCode",
+"basisOfRecord", "informationWithheld", "dataGeneralizations", "dynamicProperties")
 
-recordlevelTermsClass <- c("institutionID", "collectionID", "datasetID", "institutionCode", "collectionCode", "datasetName", "ownerInstitutionCode", 
-    "basisOfRecord", "informationWithheld", "dataGeneralizations", "dynamicProperties")
+occurenceClass <- c("occurrenceID", "catalogNumber", "occurrenceDetails", "occurrenceRemarks", "recordNumber", "recordedBy", "individualID",
+"individualCount", "sex", "lifeStage", "reproductiveCondition", "associatedTaxa", "behavior", "establishmentMeans", "occurrenceStatus",
+"preparations", "disposition", "otherCatalogNumbers", "previousIdentifications", "associatedMedia", "associatedReferences",
+"associatedOccurrences", "associatedSequences")
 
-occurenceClass <- c("occurrenceID", "catalogNumber", "occurrenceDetails", "occurrenceRemarks", "recordNumber", "recordedBy", "individualID", 
-    "individualCount", "sex", "lifeStage", "reproductiveCondition", "associatedTaxa", "behavior", "establishmentMeans", "occurrenceStatus", 
-    "preparations", "disposition", "otherCatalogNumbers", "previousIdentifications", "associatedMedia", "associatedReferences", 
-    "associatedOccurrences", "associatedSequences")
+eventClass <- c("eventID", "samplingProtocol", "samplingEffort", "eventDate", "eventTime", "startDayOfYear", "endDayOfYear", "year",
+"month", "day", "verbatimEventDate", "habitat", "fieldNumber", "fieldNotes", "eventRemarks")
 
-eventClass <- c("eventID", "samplingProtocol", "samplingEffort", "eventDate", "eventTime", "startDayOfYear", "endDayOfYear", "year", 
-    "month", "day", "verbatimEventDate", "habitat", "fieldNumber", "fieldNotes", "eventRemarks")
+locationClass <- c("locationID", "higherGeographyID", "higherGeography", "continent", "waterBody", "islandGroup", "island", "country",
+"countryCode", "stateProvince", "county", "municipality", "locality", "verbatimLocality", "verbatimElevation", "minimumElevationInMeters",
+"maximumElevationInMeters", "verbatimDepth", "minimumDepthInMeters", "maximumDepthInMeters", "minimumDistanceAboveSurfaceInMeters",
+"verbatimLatitude", "verbatimLongitude", "verbatimCoordinateSystem", "verbatimSRS", "decimalLatitude", "decimalLongitude",
+"geodeticDatum", "coordinateUncertaintyInMeters", "coordinatePrecision", "pointRadiusSpatialFit", "footprintWKT", "footprintSRS",
+"footprintSpatialFit", "georeferencedBy", "georeferencedDate", "georeferenceProtocol", "georeferenceSources", "georeferenceVerificationStatus",
+"georeferenceRemarks", "maximumDistanceAboveSurfaceInMeters", "locationAccordingTo", "locationRemarks", "verbatimCoordinates")
 
-locationClass <- c("locationID", "higherGeographyID", "higherGeography", "continent", "waterBody", "islandGroup", "island", "country", 
-    "countryCode", "stateProvince", "county", "municipality", "locality", "verbatimLocality", "verbatimElevation", "minimumElevationInMeters", 
-    "maximumElevationInMeters", "verbatimDepth", "minimumDepthInMeters", "maximumDepthInMeters", "minimumDistanceAboveSurfaceInMeters", 
-    "verbatimLatitude", "verbatimLongitude", "verbatimCoordinateSystem", "verbatimSRS", "decimalLatitude", "decimalLongitude", 
-    "geodeticDatum", "coordinateUncertaintyInMeters", "coordinatePrecision", "pointRadiusSpatialFit", "footprintWKT", "footprintSRS", 
-    "footprintSpatialFit", "georeferencedBy", "georeferencedDate", "georeferenceProtocol", "georeferenceSources", "georeferenceVerificationStatus", 
-    "georeferenceRemarks", "maximumDistanceAboveSurfaceInMeters", "locationAccordingTo", "locationRemarks", "verbatimCoordinates")
+geologicalContextClass <- c("geologicalContextID", "earliestEonOrLowestEonothem", "latestEonOrHighestEonothem", "earliestEraOrLowestErathem",
+"latestEraOrHighestErathem", "earliestPeriodOrLowestSystem", "latestPeriodOrHighestSystem", "earliestEpochOrLowestSeries",
+"latestEpochOrHighestSeries", "earliestAgeOrLowestStage", "latestAgeOrHighestStage", "lowestBiostratigraphicZone", "highestBiostratigraphicZone",
+"lithostratigraphicTerms", "group", "formation", "member", "bed")
 
+identificationClass <- c("identificationID", "identifiedBy", "dateIdentified", "identificationReferences", "identificationVerificationStatus",
+"identificationRemarks", "identificationQualifier", "typeStatus")
 
-geologicalContextClass <- c("geologicalContextID", "earliestEonOrLowestEonothem", "latestEonOrHighestEonothem", "earliestEraOrLowestErathem", 
-    "latestEraOrHighestErathem", "earliestPeriodOrLowestSystem", "latestPeriodOrHighestSystem", "earliestEpochOrLowestSeries", 
-    "latestEpochOrHighestSeries", "earliestAgeOrLowestStage", "latestAgeOrHighestStage", "lowestBiostratigraphicZone", "highestBiostratigraphicZone", 
-    "lithostratigraphicTerms", "group", "formation", "member", "bed")
+resourceRelationshipClass <- c("resourceRelationshipID", "resourceID", "relatedResourceID", "relationshipOfResource", "relationshipAccordingTo",
+"relationshipEstablishedDate", "relationshipRemarks", "relationshipAccordingTo")
 
-identificationClass <- c("identificationID", "identifiedBy", "dateIdentified", "identificationReferences", "identificationVerificationStatus", 
-    "identificationRemarks", "identificationQualifier", "typeStatus")
-
-resourceRelationshipClass <- c("resourceRelationshipID", "resourceID", "relatedResourceID", "relationshipOfResource", "relationshipAccordingTo", 
-    "relationshipEstablishedDate", "relationshipRemarks", "relationshipAccordingTo")
-
-measurementOrFactClass <- c("measurementID", "measurementType", "measurementValue", "measurementAccuracy", "measurementUnit", 
-    "measurementDeterminedBy", "measurementMethod", "measurementRemarks")
+measurementOrFactClass <- c("measurementID", "measurementType", "measurementValue", "measurementAccuracy", "measurementUnit",
+"measurementDeterminedBy", "measurementMethod", "measurementRemarks")
 
